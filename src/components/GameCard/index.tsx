9f1706e98146d5c9b6335cd91e31204dc954b095
erickhanon <erick.hanon@gmail.com>
2022-09-27 00:31:19 -0300

    Home layout finish

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,11 +1,39 @@
-import { TouchableOpacity } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  TouchableOpacity,
+  ImageBackground,
+  ImageSourcePropType,
+  TouchableOpacityProps,
+  Text,
+} from "react-native";
+import { THEME } from "../../theme";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
-export function GameCard() {
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props {
+  data: GameCardProps;
+}
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps;
+}
+
+export function GameCard({ data, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container}>
-
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground source={data.cover} style={styles.cover}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.title}>{data.name}</Text>
+          <Text style={styles.subtitle}>{data.ads} Anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
     </TouchableOpacity>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,26 @@
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 import { styles } from './styles';
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Header } from '../../components/Header';
+import { GameCard } from '../../components/GameCard';
+import { GAMES } from '../../utils/games';
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
-      <Header title="MARIANA EU TE AMO MUITO MEU AMOR" subtitle="Casa comigo?" />
+      <Header title="Encontre o seu DUO" subtitle="Ache um parceiro para jogar online!" />
+      <FlatList 
+        data={GAMES}
+        horizontal
+        contentContainerStyle={styles.contentList}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard data={item} />
+        )}
+        showsVerticalScrollIndicator={false}
+      />
+
     </View>
   );
 }
